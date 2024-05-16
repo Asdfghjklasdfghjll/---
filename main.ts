2e12ead8088240f224950075432eaacaaecbb9be
Asdfghjklasdfghjll <frasalsbhy33@gmail.com>
2024-05-16 12:27:08 +0300

    الملفات الأولية لمشروع MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(5 + 5)
+    basic.showNumber(10 - 15)
+    basic.showNumber(5 * 2)
+    basic.showNumber(20 / 2)
+})
+basic.showString("feras abdalhadi alsubhi")
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+})
